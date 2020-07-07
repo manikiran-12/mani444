9dce4baad1705cfdd17248d757de447ac02b34c1
mk19439 <mk19439@OJAS-DD11.corp.ojas-it.com>
2020-07-07 17:11:10 +0530

    add

---
+++ src/main/java/com/security/JwtExample4444Application.java
@@ -0,0 +1,13 @@
+package com.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtExample4444Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtExample4444Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/JwtExample4444ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtExample4444ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
